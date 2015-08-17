9104dd4133964563560e74acdb032a6c8ed1e738
belliture <belliture@gmail.com>
2015-08-17 18:37:41 +0900

    Use %APPDATA% for windows

--- win/ghc-mod.bat
+++ win/ghc-mod.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set LOCAL_BIN=%HOME%\AppData\Roaming\local\bin
+set LOCAL_BIN=%APPDATA%\local\bin
 for /F "usebackq" %%t in (`%LOCAL_BIN%\ghc-mod root`) do set CABAL_DIR=%%t
 for /F "usebackq" %%t in (`stack path --dist-dir`) do set STACK_DIST=%CABAL_DIR%\%%t
 set DIST=%CABAL_DIR%\dist
