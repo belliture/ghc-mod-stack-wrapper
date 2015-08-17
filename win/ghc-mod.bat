2cc9261e1d27d4f39a81e77502846932640772bc
belliture <belliture@gmail.com>
2015-08-17 14:42:21 +0900

    Use existing setup-config which is generated by stack

--- win/ghc-mod.bat
+++ win/ghc-mod.bat
@@ -1,5 +1,14 @@
 @echo off
 
-set PATH=%HOME%\AppData\Roaming\local\bin;%PATH%
-stack exec -- ghc-mod %*
+set LOCAL_BIN=%HOME%\AppData\Roaming\local\bin
+for /F "usebackq" %%t in (`%LOCAL_BIN%\ghc-mod root`) do set CABAL_DIR=%%t
+for /F "usebackq" %%t in (`stack path --dist-dir`) do set STACK_DIST=%CABAL_DIR%\%%t
+set DIST=%CABAL_DIR%\dist
+
+if exist %STACK_DIST%\setup-config if not exist %DIST% (
+    mklink /j %DIST% %STACK_DIST%
+)
+
+set PATH=%LOCAL_BIN%;%PATH%
+stack exec ghc-mod -- %*
 
